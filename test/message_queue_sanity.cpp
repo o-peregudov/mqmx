77d4e5724924f33703c7b212df194099f5fcc96e
Oleg Peregudov <oleg.peregudov@nokia.com>
2015-10-21 14:34:00 +0200

    mqmx_queue_id_type and mqmx_message_id_type are used in tests Renamed file for message_queue sanity checks

--- test/message_queue_sanity.cpp
+++ test/message_queue_sanity.cpp
@@ -4,13 +4,15 @@
 int main (int argc, const char ** argv)
 {
     using namespace mqmx;
-    message_queue queue (10);
+    const mqmx_queue_id_type defQID = 10;
+    const mqmx_message_id_type defMID = 10;
+    message_queue queue (defQID);
 
     message_queue::message_ptr_type msg (queue.pop ());
     assert ((msg.get () == nullptr) &&
 	    ("Initially queue is empty!"));
 
-    int retCode = queue.push (message_queue::message_ptr_type (new message (10, 10)));
+    int retCode = queue.push (message_queue::message_ptr_type (new message (defQID, defMID)));
     assert ((retCode == ExitStatus::Success) &&
 	    ("Push should succeed!"));
 
@@ -20,23 +22,23 @@ int main (int argc, const char ** argv)
     assert ((msg.get () == nullptr) &&
 	    ("Should be empty after move!"));
 
-    retCode = queue.push (message_queue::message_ptr_type (new message (10, 10)));
+    retCode = queue.push (message_queue::message_ptr_type (new message (defQID, defMID)));
     assert ((retCode == ExitStatus::NotSupported) &&
 	    ("Push should fail, because moved out!"));
 
     msg = queue2.pop ();
     assert ((msg.get () != nullptr) &&
 	    ("Should not be empty, because moved from existing!"));
-    assert ((msg->get_qid () == 10) &&
+    assert ((msg->get_qid () == defQID) &&
 	    ("QID should match!"));
-    assert ((msg->get_mid () == 10) &&
+    assert ((msg->get_mid () == defMID) &&
 	    ("MID should match!"));
 
     queue = std::move (queue2);
-    retCode = queue.push (message_queue::message_ptr_type (new message (10, 10)));
+    retCode = queue.push (message_queue::message_ptr_type (new message (defQID, defMID)));
     assert ((retCode == ExitStatus::Success) &&
 	    ("Push should succeed, because moved in!"));
-    retCode = queue2.push (message_queue::message_ptr_type (new message (10, 10)));
+    retCode = queue2.push (message_queue::message_ptr_type (new message (defQID, defMID)));
     assert ((retCode == ExitStatus::NotSupported) &&
 	    ("Push should fail, because moved out!"));
 
