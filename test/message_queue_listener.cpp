dad82ef0c783351d46d45ef5dc2ac1f4d6316ded
Oleg Peregudov <oleg.peregudov@nokia.com>
2015-10-28 12:24:03 +0100

    TC for listener - still in progress

--- test/message_queue_listener.cpp
+++ test/message_queue_listener.cpp
@@ -15,46 +15,60 @@ int main (int argc, const char ** argv)
     message_queue queue (defQID);
     message_queue::message_ptr_type msg (queue.pop ());
     assert ((msg.get () == nullptr) &&
-            ("Initially queue is empty!"));
+            ("Initially queue is empty"));
 
     class listener_mock : public message_queue::listener
     {
     public:
-	typedef std::tuple<const queue_id_type,
-			   message_queue *,
-			   const MQNotification> notification_rec;
-	typedef std::vector<notification_rec>    notification_list;
+        typedef std::tuple<const queue_id_type,
+                           message_queue *,
+                           const MQNotification> notification_rec;
+        typedef std::vector<notification_rec>    notification_list;
 
     private:
-	notification_list _notifications;
-	
+        notification_list _notifications;
+
     public:
-	listener_mock ()
-	    : message_queue::listener ()
-	    , _notifications ()
-	{ }
+        listener_mock ()
+            : message_queue::listener ()
+            , _notifications ()
+        { }
 
-	virtual ~listener_mock ()
-	{ }
+        virtual ~listener_mock ()
+        { }
 
-	virtual void notify (const queue_id_type qid,
-			     message_queue * mq,
-			     const MQNotification nid) noexcept override
-	{
-	    _notifications.push_back (std::make_tuple (qid, mq, nid));
-	}
+        virtual void notify (const queue_id_type qid,
+                             message_queue * mq,
+                             const MQNotification nid) noexcept override
+        {
+            _notifications.push_back (std::make_tuple (qid, mq, nid));
+        }
+
+        const notification_list & get_notifications () const
+        {
+            return _notifications;
+        }
+
+        void clear_notifications ()
+        {
+            _notifications.clear ();
+        }
     };
 
-    listener_mock sampleListener;
-    status_code retCode = queue.set_listener (sampleListener);
+    listener_mock sample_listener;
+    status_code retCode = queue.set_listener (sample_listener);
     assert ((retCode == ExitStatus::Success) &&
-            ("Listener should be registered!"));
+            ("Listener should be registered"));
 
     /*
      * push operation
      */
     retCode = queue.push (message_queue::message_ptr_type (new message (defQID, defMID)));
     assert ((retCode == ExitStatus::Success) &&
-            ("Push should succeed!"));
+            ("Push should succeed"));
+    assert ((sample_listener.get_notifications ().size () == 1) &&
+	    ("Single notification should be delivered"));
+    assert ((std::get<2> (sample_listener.get_notifications ().back ()) == MQNotification::NewMessage) &&
+	    ("Proper notification should be delivered"));
     return 0;
 }
