8290ab37c9889dd390c93bb415812d8c5bdd94a9
Oleg Peregudov <oleg.peregudov@nokia.com>
2015-10-30 14:44:30 +0100

    TC for intially signaled queues

--- test/message_queue_poll.cpp
+++ test/message_queue_poll.cpp
@@ -29,17 +29,32 @@ struct test_fixture : poll_fixture
 {
     mqmx::message_queue_poll sut;
 
-    void sanity_check ()
+    void sanity_test ()
     {
 	auto mqlist = sut.poll (std::begin (mq), std::end (mq));
 	assert ((mqlist.empty () == true) &&
 		("No events should be reported"));
+    }
+
+    void initial_notification_test ()
+    {
+	const size_t stride = 3;
+	size_t nqueues_signaled = 0;
+	for (size_t ix = 0; ix < NQUEUES; ix += stride)
+	{
+	    mq[ix]->push (mqmx::message_queue::message_ptr_type (
+			      new mqmx::message (mq[ix]->get_id (), 0)));
+	    ++nqueues_signaled;
+	}
+	auto mqlist = sut.poll (std::begin (mq), std::end (mq));
+	assert ((mqlist.size () == nqueues_signaled) &&
+		("Number of signaled queues should be correct"));
     }
 };
 
 int main (int argc, const char ** argv)
 {
     test_fixture fixture;
-    fixture.sanity_check ();
+    fixture.sanity_test ();
     return 0;
 }
