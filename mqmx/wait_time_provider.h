6fb9b2065c8dc934965ac4c38384f420e2211355
Oleg Peregudov <oleg.peregudov@nokia.com>
2015-10-30 15:22:44 +0100

    wait_time_provider constructor for relative time fixed

--- mqmx/wait_time_provider.h
+++ mqmx/wait_time_provider.h
@@ -10,70 +10,71 @@ namespace mqmx
     class wait_time_provider final
     {
     public:
-	typedef std::chrono::steady_clock clock_type;
-	typedef clock_type::duration      duration_type;
-	typedef clock_type::time_point    time_point_type;
+        typedef std::chrono::steady_clock clock_type;
+        typedef clock_type::duration      duration_type;
+        typedef clock_type::time_point    time_point_type;
 
-	static const infinite_wait_time WAIT_INFINITELY;
+        static const infinite_wait_time WAIT_INFINITELY;
 
     private:
-	const duration_type   _rel_time;
-	const time_point_type _abs_time;
-	const bool            _wait_infinitely;
+        const duration_type   _rel_time;
+        const time_point_type _abs_time;
+        const bool            _wait_infinitely;
 
-	time_point_type get_now () const
-	{
-	    return clock_type::now ();
-	}
+        time_point_type get_now () const
+        {
+            return clock_type::now ();
+        }
 
     public:
-	wait_time_provider ()
-	    : _rel_time ()
-	    , _abs_time ()
-	    , _wait_infinitely (false)
-	{ }
+        wait_time_provider ()
+            : _rel_time ()
+            , _abs_time ()
+            , _wait_infinitely (false)
+        { }
 
-	wait_time_provider (const infinite_wait_time &)
-	    : _rel_time ()
-	    , _abs_time ()
-	    , _wait_infinitely (true)
-	{ }
+        wait_time_provider (const infinite_wait_time &)
+            : _rel_time ()
+            , _abs_time ()
+            , _wait_infinitely (true)
+        { }
 
-	wait_time_provider (const duration_type & rel_time)
-	    : _rel_time (rel_time)
-	    , _abs_time ()
-	    , _wait_infinitely (false)
-	{ }
+        template <class Rep, class Period>
+        wait_time_provider (const std::chrono::duration<Rep, Period> rel_time)
+            : _rel_time (rel_time)
+            , _abs_time ()
+            , _wait_infinitely (false)
+        { }
 
-	wait_time_provider (const time_point_type & abs_time)
-	    : _rel_time ()
-	    , _abs_time (abs_time)
-	    , _wait_infinitely (false)
-	{ }
+        wait_time_provider (const time_point_type & abs_time)
+            : _rel_time ()
+            , _abs_time (abs_time)
+            , _wait_infinitely (false)
+        { }
 
-	bool wait_infinitely () const
-	{
-	    return _wait_infinitely;
-	}
+        bool wait_infinitely () const
+        {
+            return _wait_infinitely;
+        }
 
-	time_point_type get_time_point () const
-	{
-	    return get_time_point (*this);
-	}
+        time_point_type get_time_point () const
+        {
+            return get_time_point (*this);
+        }
 
-	template <class reference_clock_provider>
-	time_point_type get_time_point (const reference_clock_provider & rcp) const
-	{
-	    if ((_abs_time.time_since_epoch ().count () == 0) &&
-		(_rel_time.count () == 0))
-	    {
-		return time_point_type ();
-	    }
+        template <class reference_clock_provider>
+        time_point_type get_time_point (const reference_clock_provider & rcp) const
+        {
+            if ((_abs_time.time_since_epoch ().count () == 0) &&
+                (_rel_time.count () == 0))
+            {
+                return time_point_type ();
+            }
 
-	    return ((_abs_time.time_since_epoch ().count () == 0)
-		    ? (rcp.get_now () + _rel_time)
-		    : _abs_time);
-	}
+            return ((_abs_time.time_since_epoch ().count () == 0)
+                    ? (rcp.get_now () + _rel_time)
+                    : _abs_time);
+        }
     };
 } /* namespace mqmx */
 #endif /* MQMX_WAIT_TIME_PROVIDER_H_INCLUDED */
