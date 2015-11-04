ef41f6ee4d5bbb7d8076c41512141dae8d72e8e2
Oleg Peregudov <oleg.peregudov@nokia.com>
2015-11-04 11:08:16 +0100

    Test files are renamed into CamelCase

--- test/MessageQueueListener.cpp
+++ test/MessageQueueListener.cpp
@@ -1,5 +1,4 @@
-#include "mqmx/message_queue.h"
-#include "test/stub/listener.h"
+#include "test/stubs/listener.h"
 
 #include <cassert>
 
@@ -13,18 +12,18 @@ int main (int argc, const char ** argv)
      * listener should have longer lifetime than message_queue
      * to check "Closed" notification from MQ
      */
-    stub::listener slistener;
+    stubs::listener slistener;
 
     {
 	/*
 	 * default constructor
 	 */
-	message_queue queue (defQID);
-	message_queue::message_ptr_type msg (queue.pop ());
+	MessageQueue queue (defQID);
+	MessageQueue::message_ptr_type msg (queue.pop ());
 	assert ((msg.get () == nullptr) &&
 		("Initially queue is empty"));
 
-	status_code retCode = queue.set_listener (slistener);
+	status_code retCode = queue.setListener (slistener);
 	assert ((retCode == ExitStatus::Success) &&
 		("Listener should be registered"));
 
@@ -32,7 +31,7 @@ int main (int argc, const char ** argv)
 	 * push operation
 	 */
 	retCode = queue.push (
-	    message_queue::message_ptr_type (new message (defQID, defMID)));
+	    MessageQueue::message_ptr_type (new Message (defQID, defMID)));
 	assert ((retCode == ExitStatus::Success) &&
 		("Push should succeed"));
 	assert ((slistener.get_notifications ().size () == 1) &&
@@ -40,7 +39,7 @@ int main (int argc, const char ** argv)
 	assert ((std::get<1> (slistener.get_notifications ().front ()) == &queue) &&
 		("'NewData' notification flag should be delivered from proper MQ"));
 	assert (((std::get<2> (slistener.get_notifications ().front ()) &
-		  message_queue::NotificationFlag::NewData) != 0) &&
+		  MessageQueue::NotificationFlag::NewData) != 0) &&
 		("'NewData' notification flag should be delivered"));
 
 	slistener.clear_notifications ();
@@ -52,7 +51,7 @@ int main (int argc, const char ** argv)
     assert ((slistener.get_notifications ().size () == 1) &&
 	    ("Still single notification should be delivered"));
     assert (((std::get<2> (slistener.get_notifications ().front ()) &
-		       message_queue::NotificationFlag::Closed) != 0) &&
+	      MessageQueue::NotificationFlag::Closed) != 0) &&
 	     ("'Closed' notification flag should be delivered"));
     assert ((std::get<1> (slistener.get_notifications ().front ()) == nullptr) &&
             ("'Closed' notification should be delivered for nullptr"));
