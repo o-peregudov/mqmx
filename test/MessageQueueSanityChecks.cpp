2fc03ac2fa9d7581e7885c165f703c6b51804b3d
Oleg Peregudov <oleg.peregudov@nokia.com>
2016-04-22 15:12:52 +0200

    Sanity checks are now done in GTest

--- test/MessageQueueSanityChecks.cpp
+++ test/MessageQueueSanityChecks.cpp
@@ -1,80 +1,51 @@
-#if defined (NDEBUG)
-#  undef NDEBUG
-#endif
 #include <mqmx/MessageQueue.h>
-#include <cassert>
+#include <gmock/gmock.h>
 
-int main (int argc, const char ** argv)
+TEST (message_queue, sanity_checks)
 {
     using namespace mqmx;
     const queue_id_type defQID = 10;
     const message_id_type defMID = 10;
 
-    /*
-     * default constructor
-     */
     MessageQueue queue (defQID);
     Message::upointer_type msg (queue.pop ());
-    assert ((msg.get () == nullptr) &&
-            ("Initially queue is empty!"));
+    ASSERT_EQ (nullptr, msg.get ());
 
-    /*
-     * push operation sanity - empty message
-     */
     status_code retCode = queue.push (nullptr);
-    assert ((retCode == ExitStatus::InvalidArgument) &&
-            ("Push should fail!"));
+    ASSERT_EQ (ExitStatus::InvalidArgument, retCode);
 
-    /*
-     * push operation sanity - message from other queue
-     */
     {
 	MessageQueue queue2 (defQID + 1);
 	retCode = queue.push (queue2.newMessage<Message> (defQID));
-	assert ((retCode == ExitStatus::NotSupported) &&
-		("Push should fail!"));
+	ASSERT_EQ (ExitStatus::NotSupported, retCode);
     }
 
-    /*
-     * push operation
-     */
     retCode = queue.enqueue<Message> (defMID);
-    assert ((retCode == ExitStatus::Success) &&
-            ("Push should succeed!"));
+    ASSERT_EQ (ExitStatus::Success, retCode);
+}
 
-    /*
-     * MQ exhaustion
-     */
-    msg = queue.pop ();
-    assert ((msg.get () != nullptr) &&
-            ("Should not be empty!"));
-    assert ((msg->getQID () == defQID) &&
-            ("QID should match!"));
-    assert ((msg->getMID () == defMID) &&
-            ("MID should match!"));
+TEST (message_queue, fifo_ordering)
+{
+    using namespace mqmx;
+    const queue_id_type defQID = 10;
+    const message_id_type defMID = 10;
 
-    msg = queue.pop ();
-    assert ((msg.get () == nullptr) &&
-            ("Should be empty!"));
-
-    /*
-     * FIFO message ordering
-     */
+    MessageQueue queue (defQID);
     for (size_t ix = 0; ix < 10; ++ix)
     {
-        retCode = queue.enqueue<Message> (defMID + ix);
-        assert ((retCode == ExitStatus::Success) &&
-                ("Push should succeed!"));
+	status_code retCode = queue.enqueue<Message> (defMID + ix);
+	ASSERT_EQ (ExitStatus::Success, retCode);
     }
+
+    Message::upointer_type msg;
     for (size_t ix = 0; ix < 10; ++ix)
     {
         msg = queue.pop ();
-        assert ((msg.get () != nullptr) &&
-                ("Should not be empty!"));
-        assert ((msg->getQID () == defQID) &&
-                ("QID should match!"));
-        assert ((msg->getMID () == (defMID + ix)) &&
-                ("MID should match!"));
+	ASSERT_NE (nullptr, msg.get ());
+	ASSERT_EQ (defQID, msg->getQID ());
+	ASSERT_EQ ((defMID + ix), msg->getMID ());
     }
-    return 0;
+
+    msg = queue.pop ();
+    ASSERT_EQ (nullptr, msg.get ());
 }
