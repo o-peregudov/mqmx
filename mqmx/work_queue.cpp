eb4c38b9b9004406837634b984c19da4a2926bfa
O.Peregudov <o.peregudov@gmail.com>
2016-08-24 16:50:52 +0200

    work_queue: class added

---
+++ mqmx/work_queue.cpp
@@ -0,0 +1,328 @@
+#include <mqmx/work_queue.h>
+#include <algorithm>
+
+namespace mqmx
+{
+    const work_queue::client_id_type work_queue::INVALID_CLIENT_ID =
+        static_cast<work_queue::client_id_type> (-1);
+    const work_queue::work_id_type work_queue::INVALID_WORK_ID =
+        static_cast<work_queue::work_id_type> (-1);
+    const work_queue::duration_type work_queue::RUN_ONCE =
+        work_queue::duration_type ();
+
+    work_queue::wq_item::wq_item ()
+        : time_point ()
+        , client_id (INVALID_CLIENT_ID)
+        , work ()
+        , period ()
+    { }
+
+    work_queue::wq_item::wq_item (
+        const work_queue::time_point_type & tpoint,
+        const work_queue::client_id_type clientid,
+        const work_queue::work_pointer_type & pwork,
+        const work_queue::duration_type & tperiod)
+        : time_point (tpoint)
+        , client_id (clientid)
+        , work (pwork)
+        , period (tperiod)
+    { }
+
+    work_queue::work_queue ()
+        : _next_work_id (INVALID_WORK_ID)
+        , _next_client_id (INVALID_CLIENT_ID)
+        , _mutex ()
+        , _container_change_condition ()
+        , _wq_item_container ()
+        , _container_change_flag (false)
+        , _worker_stopped_flag (false)
+        , _worker ([this]{ this->worker (); })
+    {
+    }
+
+    work_queue::~work_queue ()
+    {
+        kill_worker ();
+    }
+
+    std::pair<status_code, work_queue::work_id_type> work_queue::post_work (
+        work_queue::lock_type & guard,
+        work_queue::wq_item item)
+    {
+        if (_worker_stopped_flag)
+            return std::make_pair (ExitStatus::NotAllowed, INVALID_WORK_ID);
+
+        if (++_next_work_id == INVALID_WORK_ID)
+            ++_next_work_id;
+
+        _wq_item_container.push_back (std::make_pair (item, _next_work_id));
+        std::push_heap (std::begin (_wq_item_container),
+                        std::end (_wq_item_container),
+                        record_compare ());
+
+        signal_container_change (guard);
+        return std::make_pair(ExitStatus::Success, _next_work_id);
+    }
+
+    bool work_queue::is_idle () const
+    {
+        lock_type guard (_mutex);
+
+        return is_container_empty (guard);
+    }
+
+    std::pair<status_code, work_queue::work_id_type> work_queue::schedule_work (
+        const client_id_type client_id,
+        const work_pointer_type & work,
+        const time_point_type & start_time,
+        const duration_type & repeat_timeout)
+    {
+        if ((client_id == INVALID_CLIENT_ID) || !work)
+            return std::make_pair (ExitStatus::InvalidArgument, INVALID_WORK_ID);
+
+        time_point_type stime =
+            is_time_point_empty (start_time) ? get_current_time_point () : start_time;
+
+        lock_type guard (_mutex);
+
+        return post_work (guard, {stime, client_id, work, repeat_timeout});
+    }
+
+    bool work_queue::signal_worker_to_stop ()
+    {
+        lock_type guard (_mutex);
+
+        if (_worker_stopped_flag)
+            return false;
+
+        _wq_item_container.clear ();
+        post_work (guard, wq_item ());
+        _worker_stopped_flag = true;
+        return true;
+    }
+
+    void work_queue::reset_container_change_flag (work_queue::lock_type &)
+    {
+        _container_change_flag = false;
+    }
+
+    void work_queue::signal_container_change (work_queue::lock_type &)
+    {
+        _container_change_flag = true;
+        _container_change_condition.notify_one ();
+    }
+
+    void work_queue::kill_worker ()
+    {
+        if (signal_worker_to_stop () && _worker.joinable ())
+            _worker.join ();
+    }
+
+    void work_queue::make_heap_and_notify_worker (work_queue::lock_type & guard)
+    {
+        std::make_heap (std::begin (_wq_item_container),
+                        std::end (_wq_item_container),
+                        record_compare ());
+        signal_container_change (guard);
+    }
+
+    status_code work_queue::update_work (
+        const work_id_type work_id,
+        const client_id_type client_id,
+        const work_pointer_type & work,
+        const time_point_type & start_time,
+        const duration_type & repeat_timeout)
+    {
+        if ((client_id == INVALID_CLIENT_ID) || !work)
+            return ExitStatus::InvalidArgument;
+
+        lock_type guard (_mutex);
+
+        if (_worker_stopped_flag)
+            return ExitStatus::NotAllowed;
+
+        for (auto & elem : _wq_item_container)
+        {
+            if (elem.second == work_id)
+            {
+                elem.first = {start_time, client_id, work, repeat_timeout};
+                make_heap_and_notify_worker (guard);
+                return ExitStatus::Success;
+            }
+        }
+
+        return ExitStatus::NotFound;
+    }
+
+    status_code work_queue::cancel_work (const work_queue::work_id_type work_id)
+    {
+        lock_type guard (_mutex);
+
+        if (_worker_stopped_flag)
+            return ExitStatus::NotAllowed;
+
+        for (auto & elem : _wq_item_container)
+        {
+            if (elem.second == work_id)
+            {
+                std::swap (elem, _wq_item_container.back ());
+                _wq_item_container.pop_back ();
+
+                make_heap_and_notify_worker (guard);
+                return ExitStatus::Success;
+            }
+        }
+
+        return ExitStatus::NotFound;
+    }
+
+    work_queue::time_point_type work_queue::get_nearest_time_point () const
+    {
+        lock_type guard (_mutex);
+
+        if (is_container_empty (guard))
+            return get_empty_time_point ();
+
+        return _wq_item_container.front ().first.time_point;
+    }
+
+    void work_queue::signal_going_to_idle (work_queue::lock_type &)
+    {
+        /* derived class can use this */
+    }
+
+    bool work_queue::wait_for_some_work (work_queue::lock_type & guard)
+    {
+        if (is_container_empty (guard))
+            signal_going_to_idle (guard);
+
+        _container_change_condition.wait (guard, [&]{ return !is_container_empty (guard); });
+        return static_cast<bool> (_wq_item_container.front ().first.work);
+    }
+
+    bool work_queue::get_container_change_flag (work_queue::lock_type &) const
+    {
+        return _container_change_flag;
+    }
+
+    bool work_queue::wait_for_time_point (
+        work_queue::lock_type & guard, const work_queue::time_point_type & timepoint)
+    {
+        if (_container_change_condition.wait_until (guard, timepoint,
+                                   [&]{
+                                       return get_container_change_flag (guard);
+                                   }))
+        {
+            reset_container_change_flag (guard);
+            return false;
+        }
+        return true;
+    }
+
+    work_queue::time_point_type work_queue::execute_work (
+        lock_type &, const work_queue::record_type & rec)
+    {
+        auto rescheduled_work_time_point = get_empty_time_point ();
+        try
+        {
+            rec.first.work (rec.second);
+
+            const bool rescheduling_needed = (0 < rec.first.period.count ());
+            if (rescheduling_needed)
+                rescheduled_work_time_point = rec.first.time_point + rec.first.period;
+        }
+        catch (...)
+        {
+        }
+        return rescheduled_work_time_point;
+    }
+
+    work_queue::time_point_type work_queue::get_empty_time_point () const
+    {
+        return time_point_type ();
+    }
+
+    bool work_queue::is_time_point_empty (const work_queue::time_point_type & tp) const
+    {
+        return (0 == tp.time_since_epoch ().count ());
+    }
+
+    void work_queue::worker ()
+    {
+        lock_type guard (_mutex);
+        for (;;)
+        {
+            if (!wait_for_some_work (guard))
+            {
+                // got termination notification - exit thread
+                _wq_item_container.clear ();
+                break;
+            }
+
+            if (!wait_for_time_point (guard, _wq_item_container.front ().first.time_point) ||
+                is_container_empty (guard))
+            {
+                // timer queue has been changed - we have to restart the loop
+                continue;
+            }
+
+            std::pop_heap (std::begin (_wq_item_container), std::end (_wq_item_container),
+                           record_compare ());
+            record_type item = std::move (_wq_item_container.back ());
+            _wq_item_container.pop_back ();
+
+            const auto rescheduled_work_time_point = execute_work (guard, item);
+            if (!is_time_point_empty (rescheduled_work_time_point))
+            {
+                item.first.time_point = rescheduled_work_time_point;
+                _wq_item_container.push_back (std::move (item));
+                std::push_heap (std::begin (_wq_item_container),
+                                std::end (_wq_item_container),
+                                record_compare ());
+            }
+        }
+    }
+
+    status_code work_queue::cancel_client_works (const work_queue::client_id_type client_id)
+    {
+        lock_type guard (_mutex);
+
+        if (_worker_stopped_flag)
+            return ExitStatus::NotAllowed;
+
+        auto is_client_predicate = [client_id](const record_type & r){
+            return (r.first.client_id == client_id);
+        };
+
+        container_type::iterator new_end =
+            std::remove_if (std::begin (_wq_item_container),
+                            std::end (_wq_item_container),
+                            is_client_predicate);
+        if (new_end == std::end (_wq_item_container))
+            return ExitStatus::NotFound;
+
+        _wq_item_container.erase (new_end, _wq_item_container.end ());
+        make_heap_and_notify_worker (guard);
+        return ExitStatus::Success;
+    }
+
+    work_queue::client_id_type work_queue::get_client_id ()
+    {
+        lock_type guard (_mutex);
+
+        if (++_next_client_id == INVALID_CLIENT_ID)
+            ++_next_client_id;
+
+        return _next_client_id;
+    }
+
+    work_queue::time_point_type work_queue::get_current_time_point () const
+    {
+        return clock_type::now ();
+    }
+
+    bool work_queue::is_container_empty (work_queue::lock_type &) const
+    {
+        return _wq_item_container.empty ();
+    }
+} /* namespace mqmx */
---
+++ mqmx/work_queue.h
@@ -0,0 +1,278 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <thread>
+#include <functional>
+
+#include <crs/mutex.h>
+#include <crs/condition_variable.h>
+
+#include <mqmx/libexport.h>
+#include <mqmx/types.h>
+#include <mqmx/wait_time_provider.h>
+
+namespace mqmx
+{
+    /**
+     * This is generic work queue (WQ).
+     *
+     * Main idea behind this implementation is to execute user work
+     * at specified moment of time (asynchronously).
+     *
+     * Class is non-copyable but movable (yet move constructor/operator
+     * are not implemented).
+     *
+     * \note Because of implementation details very short timeout values
+     *       (usually less than 10 ms) cannot be guaranteed.
+     *       Actual precision was not estimated.
+     */
+    class MQMX_EXPORT work_queue
+    {
+        work_queue (const work_queue &) = delete;
+        work_queue & operator = (const work_queue &) = delete;
+
+    public:
+        using mutex_type        = crs::mutex_type;
+        using lock_type         = crs::lock_type;
+        using condvar_type      = crs::condvar_type;
+        using thread_type       = std::thread;
+        using clock_type        = wait_time_provider::clock_type;
+        using time_point_type   = clock_type::time_point;
+        using duration_type     = clock_type::duration;
+        using upointer_type     = std::unique_ptr<work_queue>;
+        using client_id_type    = unsigned long;
+        using work_id_type      = unsigned long;
+        using work_pointer_type = std::function<void (const work_id_type)>;
+
+        static const client_id_type INVALID_CLIENT_ID;
+        static const work_id_type   INVALID_WORK_ID;
+	static const duration_type  RUN_ONCE;
+
+    protected:
+        /**
+         * Basic data structure used by internal WQ implementation
+         *
+         * Contains the following fields:
+         *  - time_point (when the job should be executed)
+         *  - pointer to a work function
+         *  - duration of periodic invocation (if this functionality
+         *    is required, if not - work will be executed only once)
+         *  - ID of the work's owner
+         */
+        struct wq_item
+        {
+            time_point_type   time_point;
+            client_id_type    client_id;
+            work_pointer_type work;
+            duration_type     period;
+
+            wq_item ();
+            wq_item (const time_point_type & tpoint,
+                     const client_id_type clientid,
+                     const work_pointer_type & pwork,
+                     const duration_type & tperiod);
+	    wq_item (wq_item &&) = default;
+	    wq_item (const wq_item &) = default;
+	    wq_item & operator = (wq_item &&) = default;
+	    wq_item & operator = (const wq_item &) = default;
+        };
+
+        typedef std::pair<wq_item, work_id_type> record_type;
+        typedef std::vector<record_type>         container_type;
+
+        /**
+         * Custom comparator used by internal WQ implementation
+         *
+         * STL priority queue requires invert implementation and
+         * this class guarantees the necessary ordering.
+         */
+        struct record_compare
+        {
+            bool operator () (const record_type & a, const record_type & b) const
+            {
+                return b.first.time_point < a.first.time_point;
+            }
+        };
+
+    public:
+        /**
+         * Default constructor
+         *
+         * Initializes all internal data members and starts
+         * internal worker thread.
+         */
+        work_queue ();
+
+        /**
+         * Destructor
+         *
+         * Terminates internal worker thread and clears all
+         * internal data members.
+         *
+         * \remark Despite the fact internal worker thread is terminated
+         *         automatically it is always a good idea to stop it
+         *         explicitly before destruction of instance of WQ
+         */
+        virtual ~work_queue ();
+
+        /**
+         * Checks, whether worker thread is waiting for any work to be
+         * scheduled (i.e. internal queue is empty) or not.
+         */
+        bool is_idle () const;
+
+        /**
+         * Terminates worker thread immediately, but gently
+         *
+         * All works, which were present in queue, but were not
+         * processed will be discarded.
+         *
+         * \note
+         *  - you cannot put new works into queue after this call;
+         *  - this is kind of final call, because there are no possibility to
+         *    restart (recreate) internal worker thread.
+         */
+        void kill_worker ();
+
+        /**
+         * Schedule new work for asynchronous execution
+         *
+         * \param work is a poiter to function, that should be executed
+         * \param client_id is an ID, aimed to group work items which belongs to
+         *        some particular client
+         * \param start_time is a time point, when work execution
+         *        should be triggered for the first time
+         * \param repeat_timeout is a timeout, that is used to periodically
+         *        re-trigger work execution since first trigger (it can be set
+         *        to 0 if periodic triggering is not needed)
+         *
+         * \returns <i>ExitStatus::InvalidArgument</i> if work pointer
+         *          is null
+         * \returns <i>ExitStatus::NotAllowed</i> if worker thread
+         *          is terminated
+         * \returns <i>ExitStatus::Success</i> in case of success
+         */
+        std::pair<status_code, work_id_type> schedule_work (
+            const client_id_type client_id,
+            const work_pointer_type & work,
+            const time_point_type & start_time = time_point_type (),
+            const duration_type & repeat_timeout = RUN_ONCE);
+
+        /**
+         * Update work with given ID.
+         *
+         * With this call you can change work item completely -- existing
+         * record will be replaced with new one, only ID of event will be
+         * preserved.
+         *
+         * \param work_id is the ID of already posted work to be altered
+         * \param work is a poiter to function, that should be executed
+         * \param client_id is an ID, aimed to group work items which belongs to
+         *        some particular client
+         * \param start_time is a time point, when work should be triggered
+         *        for the first time
+         * \param repeat_timeout is a timeout, that is used to periodically
+         *        re-trigger work execution since first trigger (it can be set
+         *        to 0 if periodic triggering is not needed)
+         *
+         * \returns <i>ExitStatus::InvalidArgument</i> if work pointer
+         *          is null
+         * \returns <i>ExitStatus::NotAllowed</i> if worker thread
+         *          is terminated
+         * \returns <i>ExitStatus::NotFound</i> if work with given ID
+         *          is not in the queue
+         * \returns <i>ExitStatus::Success</i> in case of success
+         */
+        status_code update_work (const work_id_type work_id,
+                                 const client_id_type client_id,
+                                 const work_pointer_type & work,
+                                 const time_point_type & start_time,
+                                 const duration_type & repeat_timeout);
+
+        /**
+         * Cancel timer event with given ID
+         *
+         * \param work_id is the ID of already posted work to be canceled
+         *
+         * \returns <i>ExitStatus::NotAllowed</i> if worker thread
+         *          is terminated
+         * \returns <i>ExitStatus::NotFound</i> if work with given ID
+         *          is not in the queue
+         * \returns <i>ExitStatus::Success</i> in case of success
+         */
+        status_code cancel_work (const work_id_type work_id);
+
+        /**
+         * Cancel all work items, which belongs to specified owner
+         *
+         * \returns <i>ExitStatus::NotAllowed</i> if worker thread
+         *          is terminated
+         * \returns <i>ExitStatus::NotFound</i> if no events were listed
+         *          in parameter
+         * \returns <i>ExitStatus::Success</i> if events were removed from
+         *          the queue
+         */
+        status_code cancel_client_works (const client_id_type client_id);
+
+        /**
+         * Get unique client ID
+         *
+         * Unique client ID is needed for grouping work item by client/owner.
+         */
+        client_id_type get_client_id ();
+
+        /**
+         * Get nearest time point
+         *
+         * \returns nearest time point or empty time point (set to epoch)
+         *          if there are no events scheduled
+         *
+         * \note This method is in general needed for tests.
+         */
+        time_point_type get_nearest_time_point () const;
+
+        /**
+         * Get current time point
+         *
+         * \returns clock_type::now ()
+         */
+        virtual time_point_type get_current_time_point () const;
+
+    protected:
+        bool wait_for_some_work (lock_type &);
+
+        void reset_container_change_flag (lock_type &);
+        bool get_container_change_flag (lock_type &) const;
+
+        bool is_container_empty (lock_type &) const;
+        bool is_time_point_empty (const time_point_type &) const;
+
+        time_point_type get_empty_time_point () const;
+
+        virtual void signal_going_to_idle (lock_type &);
+        virtual bool wait_for_time_point (lock_type &, const time_point_type &);
+        virtual time_point_type execute_work (lock_type &, const record_type &);
+
+    private:
+        std::pair<status_code, work_id_type> post_work (lock_type &, wq_item);
+
+        void signal_container_change (lock_type &);
+        void make_heap_and_notify_worker (lock_type &);
+        bool signal_worker_to_stop ();
+        void worker ();
+
+        work_id_type       _next_work_id;
+        client_id_type     _next_client_id;
+
+    protected:
+        mutable mutex_type _mutex;
+        condvar_type       _container_change_condition;
+
+    private:
+        container_type     _wq_item_container;
+        bool               _container_change_flag;
+        bool               _worker_stopped_flag;
+        thread_type        _worker;
+    };
+} /* namespace mqmx */
