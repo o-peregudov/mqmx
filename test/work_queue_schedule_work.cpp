6b85a4f92e60db56ef6bd586c60a50709d267f44
O.Peregudov <o.peregudov@gmail.com>
2016-09-06 17:13:54 +0200

    work_queue: schedule_work TC
    Post condition validated after 1 sec timeout.

---
+++ test/work_queue_schedule_work.cpp
@@ -0,0 +1,55 @@
+#include "mqmx/work_queue.h"
+#include "test/FakeIt/single_header/standalone/fakeit.hpp"
+#include <crs/semaphore.h>
+
+#undef NDEBUG
+#include <cassert>
+
+struct work_interface
+{
+    virtual void do_something (const mqmx::work_queue::work_id_type) = 0;
+    virtual ~work_interface () { }
+};
+
+int main ()
+{
+    using namespace mqmx;
+    using namespace fakeit;
+    using mock_type = Mock<work_interface>;
+
+    mock_type mock;
+    crs::semaphore sem;
+    When (Method (mock, do_something)).Do(
+        [&sem](const mqmx::work_queue::work_id_type)
+        {
+            sem.post ();
+        });
+    {
+        work_queue sut;
+
+        status_code ec = ExitStatus::Success;
+        work_queue::work_id_type work_id = work_queue::INVALID_WORK_ID;
+        work_queue::client_id_type client_id = sut.get_client_id ();
+
+        std::tie (ec, work_id) = sut.schedule_work (
+            client_id,
+            std::bind (&work_interface::do_something,
+                       &(mock.get ()),
+                       std::placeholders::_1));
+
+        assert (ec == ExitStatus::Success);
+        assert (work_id != work_queue::INVALID_WORK_ID);
+
+        assert (sem.wait_for (std::chrono::seconds (1)));
+        std::this_thread::yield ();
+
+        ec = sut.cancel_work (work_id);
+        assert (ec == ExitStatus::NotFound);
+
+        Verify (Method (mock, do_something).Using (work_id))
+            .Once ();
+    }
+    VerifyNoOtherInvocations (mock);
+
+    return 0;
+}
