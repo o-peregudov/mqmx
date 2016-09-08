e42e9780bc8f0a64d3cecd66433fa5b9a36a2bd1
O.Peregudov <o.peregudov@gmail.com>
2016-09-08 14:33:28 +0200

    work_queue_for_tests: TC for periodic works
    Tests two classes at once - time manipulation
    methods from 'work_queue_for_tests' and part
    with work item rescheduling from 'work_queue'.
    
    Since our TC supposed to be robust and reproducible
    we cannot rely on OS scheduler and therefore this
    combined testing has been choosen.

---
+++ test/work_queue_for_tests_schedule_work_periodic.cpp
@@ -0,0 +1,71 @@
+#include "mqmx/work_queue.h"
+#include "test/FakeIt/single_header/standalone/fakeit.hpp"
+#include "test/helpers/work_queue_for_tests.h"
+
+#undef NDEBUG
+#include <cassert>
+
+struct work_interface
+{
+    virtual void do_something (const mqmx::work_queue::work_id_type) = 0;
+    virtual ~work_interface ()
+    {
+    }
+};
+
+struct sync_interface
+{
+    virtual void do_something (
+        const mqmx::work_queue::work_id_type,
+        const mqmx::work_queue::time_point_type) = 0;
+    virtual ~sync_interface ()
+    {
+    }
+};
+
+int main ()
+{
+    using namespace mqmx;
+    using namespace fakeit;
+    using work_mock_type = Mock<work_interface>;
+    using sync_mock_type = Mock<sync_interface>;
+
+    work_mock_type work_mock;
+    sync_mock_type sync_mock;
+    Fake (Method (work_mock, do_something));
+    Fake (Method (sync_mock, do_something));
+    {
+        helpers::work_queue_for_tests sut (
+            std::bind (&sync_interface::do_something,
+                       &(sync_mock.get ()),
+                       std::placeholders::_1,
+                       std::placeholders::_2));
+
+        status_code ec = ExitStatus::Success;
+        work_queue::work_id_type work_id = work_queue::INVALID_WORK_ID;
+        work_queue::client_id_type client_id = sut.get_client_id ();
+
+        const auto execution_period = std::chrono::milliseconds (10);
+        std::tie (ec, work_id) = sut.schedule_work (
+            client_id,
+            std::bind (&work_interface::do_something,
+                       &(work_mock.get ()),
+                       std::placeholders::_1),
+            sut.get_current_time_point () + execution_period / 2,
+            execution_period);
+
+        assert (ec == ExitStatus::Success);
+        assert (work_id != work_queue::INVALID_WORK_ID);
+
+        const size_t nrepetitions = 5;
+        sut.forward_time (nrepetitions * execution_period);
+
+        Verify (Method (work_mock, do_something).Using (work_id) +
+                Method (sync_mock, do_something).Using (work_id, _))
+            .Exactly (nrepetitions);
+    }
+    VerifyNoOtherInvocations (work_mock);
+    VerifyNoOtherInvocations (sync_mock);
+
+    return 0;
+}
